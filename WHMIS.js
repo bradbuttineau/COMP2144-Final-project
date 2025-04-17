cbcb3fd8b84ebf440a958ff4ba53a3ea1c8191f6
Brad Buttineau <113149072+bradbuttineau@users.noreply.github.com>
2025-04-17 16:57:45 -0400

    add whims.html, whmis.js and modify placment of lifting techniques

---
+++ WHMIS.js
@@ -0,0 +1,24 @@
+import './main.css'
+
+<a-entity class="link"
+geometry="primitive: plane; height: 1; width: 1"
+material="shader: flat; src: #cubes-thumb"
+sound="on: click; src: #click-sound"
+geometry="primitive: plane; height: 1; width: 1"
+material="shader: flat; src: ${thumb}"
+sound="on: click; src: #click-sound"
+event-set__mouseenter="scale: 1.2 1.2 1"
+event-set__mouseleave="scale: 1 1 1"
+event-set__click="_target: #image-360; _delay: 300; material.src: ${src}">
+
+</a-entity>
+
+
+AFRAME.registerComponent('button', {
+    init() {
+      const btn = document.getElementById('myButton')
+      btn.addEventListener('click', () => {
+        window.open('https://google.com', '_blank')
+      })
+    }
+  })
--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 import './main.css'
 
+<a-entity class="link"
+    geometry="primitive: plane; height: 1; width: 1"
+    material="shader: flat; src: #cubes-thumb"
+    sound="on: click; src: #click-sound"
+    event-set__mouseenter="scale: 1.2 1.2 1"
+    event-set__mouseleave="scale: 1 1 1"
+    event-set__click="_target: #image-360; _delay: 300; material.src: ${src}">
 
+</a-entity>
 
 
 AFRAME.registerComponent('button', {
     init() {
-      const btn = document.getElementById('myButton')
-      btn.addEventListener('click', () => {
-        window.open('https://google.com', '_blank')
-      })
+        const btn = document.getElementById('myButton')
+        btn.addEventListener('click', () => {
+            window.open('https://google.com', '_blank')
+        })
     }
-  })
+})
